c0c15f01525590e93d6d790e7c331dcff01151d0
fassdragon2 <77299176+fassdragon2@users.noreply.github.com>
2022-10-07 15:52:00 -0400

    Create scripts.lua

---
+++ scripts.lua
@@ -0,0 +1,40 @@
+Nuke Hub Most Games
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NukeVsCity/TheALLHACKLoader/main/NukeLoader"))()
+
+Hit Box Script All Games
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RectangularObject/Roblox-Hitbox-Expander/main/HitboxExtenderv2.lua",true))()
+
+Custom CrossHair All Games
+-- Custom Crosshair by zzerexx#3970
+getgenv().CrosshairSettings = {
+    Color = Color3.fromRGB(255,0,0),
+    RainbowColor = false,
+    Opacity = 1,
+    Length = 10, -- Length of each line
+    Thickness = 2, -- Thickness of each line
+    Offset = 0, -- Offset from the middle point
+    Dot = false, -- not recommended
+    FollowCursor = true, -- Crosshair follows the cursor
+    HideMouseIcon = true, -- Hides the mouse icon, set to 0 to ignore
+    HideGameCrosshair = true, -- Hides the current game's crosshair (if its supported)
+    ToggleKey = Enum.KeyCode.RightAlt, -- Toggles crosshair visibility
+} -- v1.2.1
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zzerexx/scripts/main/CustomCrosshair.lua", true))()
+
+AimWareBETA All Games
+loadstring(game:HttpGet"https://raw.githubusercontent.com/Wheeleee/AIMWARE/main/StartupBETA")()
+
+AimWare All Games
+loadstring(game:HttpGet"https://raw.githubusercontent.com/Wheeleee/AIMWARE/main/Startup")()
+
+ReJoin All Games
+local ts = game:GetService("TeleportService")
+
+local p = game:GetService("Players").LocalPlayer
+
+ 
+
+ts:Teleport(game.PlaceId, p)
+
+VapeV4 Roblox BedWars
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
